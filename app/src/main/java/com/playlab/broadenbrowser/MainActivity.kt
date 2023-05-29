da0d2513eb336d06a9fbd4d35bd863a468155ddd
João Pedro <joaoplay16@gmail.com>
2023-05-28 21:27:45 -0300

    Hide status bars and navigation bars

--- app/src/main/java/com/playlab/broadenbrowser/MainActivity.kt
+++ app/src/main/java/com/playlab/broadenbrowser/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.playlab.broadenbrowser
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.playlab.broadenbrowser.ui.theme.BroadenBrowserTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.attributes.layoutInDisplayCutoutMode =
+            WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        val windowInsetsCompat = WindowInsetsControllerCompat(
+            window,
+            window.decorView
+        )
+        windowInsetsCompat.hide(WindowInsetsCompat.Type.navigationBars())
+        windowInsetsCompat.hide(WindowInsetsCompat.Type.statusBars())
+        windowInsetsCompat.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
         setContent {
             BroadenBrowserTheme {
                 Surface(
