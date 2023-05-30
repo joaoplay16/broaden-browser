b61c490141d9a6d49f8afeac67fc1acc52908a0a
João Pedro <joaoplay16@gmail.com>
2023-05-29 23:23:29 -0300

    Create search bar component

---
+++ app/src/main/java/com/playlab/broadenbrowser/ui/components/SearchBar.kt
@@ -0,0 +1,102 @@
+package com.playlab.broadenbrowser.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.playlab.broadenbrowser.R
+import com.playlab.broadenbrowser.ui.theme.BroadenBrowserTheme
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    text: String,
+    onTextChange: (String) -> Unit,
+    onClearClick: () -> Unit,
+    onSearch: () -> Unit = {},
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = text,
+        shape = RoundedCornerShape(32.dp),
+        onValueChange = onTextChange,
+        placeholder = {
+            Text(
+                modifier = Modifier
+                    .alpha(alpha = 0.5f),
+                text = stringResource(id = R.string.search_bar_placeholder),
+                fontSize = dimensionResource(id = R.dimen.search_bar_font_size).value.sp,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface,
+            )
+        },
+        textStyle = MaterialTheme.typography.bodyMedium.copy(
+            color = MaterialTheme.colorScheme.onSurface,
+            fontSize = dimensionResource(id = R.dimen.search_bar_font_size).value.sp
+        ),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = { onSearch() }
+        ),
+        colors = TextFieldDefaults.colors(
+            cursorColor = MaterialTheme.colorScheme.primary,
+            focusedIndicatorColor = MaterialTheme.colorScheme.secondary,
+            unfocusedIndicatorColor = MaterialTheme.colorScheme.secondary.copy(0.6f),
+        ),
+        trailingIcon = {
+            if (text.isNotEmpty()) {
+                IconButton(
+                    onClick = onClearClick) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = stringResource(id = R.string.clear_icon_cd)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun SearchBarPreview() {
+    BroadenBrowserTheme {
+        Surface {
+            var text by remember {
+                mutableStateOf("")
+            }
+
+            SearchBar(
+                modifier = Modifier.fillMaxWidth(),
+                text = text,
+                onTextChange = { text = it },
+                onClearClick = { text = "" }
+            )
+        }
+    }
+}
