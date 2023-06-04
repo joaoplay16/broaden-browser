2c9dd197477781d4a971a2d92ab38e230d892c17
João Pedro <joaoplay16@gmail.com>
2023-06-04 17:06:30 -0300

    Normalize tab counter colors

--- app/src/main/java/com/playlab/broadenbrowser/ui/components/TabCounter.kt
+++ app/src/main/java/com/playlab/broadenbrowser/ui/components/TabCounter.kt
@@ -11,30 +11,52 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.playlab.broadenbrowser.ui.components.TabCounterDefaults.tabCounterColors
 import com.playlab.broadenbrowser.ui.theme.BroadenBrowserTheme
+
+
+data class TabCounterColors(
+    val containerColor: Color,
+    val borderColor: Color,
+    val textColor: Color,
+)
+
+object TabCounterDefaults {
+    @Composable
+    fun tabCounterColors(): TabCounterColors {
+        return TabCounterColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            borderColor = MaterialTheme.colorScheme.outline,
+            textColor = MaterialTheme.colorScheme.outline
+        )
+    }
+}
 
 @Composable
 fun TabCounter(
     modifier: Modifier = Modifier,
     count: Int,
+    colors: TabCounterColors = tabCounterColors()
 ) {
     Text(
         modifier = modifier
             .size(18.dp)
             .background(
-                color = MaterialTheme.colorScheme.secondaryContainer,
+                color = colors.containerColor,
                 shape = RoundedCornerShape(3.dp)
             )
             .border(
                 1.dp,
-                MaterialTheme.colorScheme.outline,
+                colors.borderColor,
                 RoundedCornerShape(3.dp)
             ),
         fontSize = 12.sp,
+        color = colors.textColor,
         textAlign = TextAlign.Center,
         text = count.toString()
     )
