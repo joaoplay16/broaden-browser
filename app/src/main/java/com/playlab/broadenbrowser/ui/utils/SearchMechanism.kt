0646d28f77ecc36806fd5580d048e05af6ee9dd3
João Pedro <joaoplay16@gmail.com>
2023-06-05 20:51:39 -0300

    Create extension function to provide search mechanism query url

---
+++ app/src/main/java/com/playlab/broadenbrowser/ui/utils/SearchMechanism.kt
@@ -0,0 +1,5 @@
+package com.playlab.broadenbrowser.ui.utils
+
+enum class SearchMechanism {
+    GOOGLE, BING, YAHOO, DUCK_DUCK_GO, BAIDU
+}
--- app/src/main/java/com/playlab/broadenbrowser/ui/utils/Util.kt
+++ app/src/main/java/com/playlab/broadenbrowser/ui/utils/Util.kt
@@ -7,4 +7,16 @@ object Util {
     fun String.isUrl(): Boolean {
         return Patterns.WEB_URL.matcher(this).matches()
     }
+
+    fun String.toSearchMechanismUrl(
+        searchMechanism: SearchMechanism = SearchMechanism.GOOGLE
+    ): String {
+        return when (searchMechanism) {
+            SearchMechanism.BING -> "https://www.bing.com/search?q=$this"
+            SearchMechanism.YAHOO -> "https://search.yahoo.com/search?p=$this"
+            SearchMechanism.BAIDU -> "https://www.baidu.com/s?wd=$this"
+            SearchMechanism.DUCK_DUCK_GO -> "https://duckduckgo.com/html/?q=$this"
+            else -> "https://www.google.com/search?q=$this"
+        }
+    }
 }
