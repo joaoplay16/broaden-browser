70eea0b5fb97a16f70676653690e208b5e2b851f
João Pedro <joaoplay16@gmail.com>
2023-06-15 20:40:48 -0300

    Create database module and provide preferences repository and data-store instances

---
+++ app/src/main/java/com/playlab/broadenbrowser/di/DatabaseModule.kt
@@ -0,0 +1,37 @@
+package com.playlab.broadenbrowser.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.playlab.broadenbrowser.data.preferences.PreferencesDataStore
+import com.playlab.broadenbrowser.repository.PreferencesRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDataStore(
+        @ApplicationContext appContext: Context
+    ): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(
+            produceFile = {
+                appContext.preferencesDataStoreFile("settings")
+        }
+    )
+
+    @Provides
+    @Singleton
+    fun providePreferencesRepository(
+        dataStore: PreferencesDataStore
+    ): PreferencesRepository =
+        PreferencesRepository(dataStore)
+}
