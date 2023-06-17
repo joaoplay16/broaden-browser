e2089aceefa8dee91bdf36542e1b5e93c0610644
João Pedro <joaoplay16@gmail.com>
2023-06-16 22:45:30 -0300

    Create browser state data class that encapsulate the browser's properties

---
+++ app/src/main/java/com/playlab/broadenbrowser/ui/screens/BrowserState.kt
@@ -0,0 +1,10 @@
+package com.playlab.broadenbrowser.ui.screens
+
+data class BrowserState(
+    val isInFullscreen: Boolean = false,
+    val isStartInFullscreenEnabled: Boolean = false,
+    val isJavascriptAllowed: Boolean = true,
+    val isDarkThemeEnabled: Boolean = false,
+    val searchMechanism: String = "",
+    val externalLink: String? = null
+)
