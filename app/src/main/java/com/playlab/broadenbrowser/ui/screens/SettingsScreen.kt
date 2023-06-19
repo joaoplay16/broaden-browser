22bb37a6db7291d5dc9c62c5cbc09754c10ea915
João Pedro <joaoplay16@gmail.com>
2023-06-18 22:08:54 -0300

    Handles the enabling event of the "Enable dark theme" option

--- app/src/main/java/com/playlab/broadenbrowser/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/playlab/broadenbrowser/ui/screens/SettingsScreen.kt
@@ -20,10 +20,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -102,10 +99,14 @@ fun SettingsScreen(
                     text = stringResource(id = R.string.enable_dark_theme),
                     style = MaterialTheme.typography.titleMedium,
                 )
-                var check by remember { mutableStateOf(false) }
-                Switch(modifier = Modifier.scale(0.7f),
-                       checked = check,
-                       onCheckedChange = { check = it })
+
+                Switch(
+                    modifier = Modifier.scale(0.7f),
+                    checked = isDarkThemeEnabled,
+                    onCheckedChange = {
+                        onEvent(UiEvent.OnEnableDarkTheme(it))
+                    }
+                )
             }
             Row(
                 modifier = Modifier.fillMaxWidth(),
