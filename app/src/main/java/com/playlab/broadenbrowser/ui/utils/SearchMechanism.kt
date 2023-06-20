4d9398224c3a627dc7ba1ba9ca2770b780c11fab
João Pedro <joaoplay16@gmail.com>
2023-06-19 21:09:04 -0300

    Create a map of search mechanisms with it's names

--- app/src/main/java/com/playlab/broadenbrowser/ui/utils/SearchMechanism.kt
+++ app/src/main/java/com/playlab/broadenbrowser/ui/utils/SearchMechanism.kt
@@ -3,3 +3,11 @@ package com.playlab.broadenbrowser.ui.utils
 enum class SearchMechanism {
     GOOGLE, BING, YAHOO, DUCK_DUCK_GO, BAIDU
 }
+
+val SearchMechanisms = mapOf(
+    SearchMechanism.GOOGLE to "Google",
+    SearchMechanism.BING to "Bing",
+    SearchMechanism.YAHOO to "Yahoo",
+    SearchMechanism.DUCK_DUCK_GO to "Duck Duck Go",
+    SearchMechanism.BAIDU to "Baidu",
+)
