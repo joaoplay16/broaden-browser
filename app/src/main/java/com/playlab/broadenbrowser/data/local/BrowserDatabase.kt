bb5e6a29c97d665414207123dfbcd8ebdfa21382
João Pedro <joaoplay16@gmail.com>
2023-06-28 22:15:50 -0300

    Marks 'BrowserDatabase' as a room database and add entities

--- app/src/main/java/com/playlab/broadenbrowser/data/local/BrowserDatabase.kt
+++ app/src/main/java/com/playlab/broadenbrowser/data/local/BrowserDatabase.kt
@@ -1,8 +1,15 @@
 package com.playlab.broadenbrowser.data.local
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.playlab.broadenbrowser.data.local.dao.BrowserDao
+import com.playlab.broadenbrowser.model.HistoryPage
+import com.playlab.broadenbrowser.model.TabPage
 
+@Database(
+    entities = [TabPage::class, HistoryPage::class],
+    version = 1
+)
 abstract class BrowserDatabase : RoomDatabase() {
     abstract fun browserDao(): BrowserDao
 }
