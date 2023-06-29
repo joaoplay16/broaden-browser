4aee8febaaadc91fd5a559855cc94b2011046133
João Pedro <joaoplay16@gmail.com>
2023-06-28 21:51:43 -0300

    Add database tables 'tabs' and 'history'

--- app/src/main/java/com/playlab/broadenbrowser/ui/utils/Constants.kt
+++ app/src/main/java/com/playlab/broadenbrowser/ui/utils/Constants.kt
@@ -2,4 +2,6 @@ package com.playlab.broadenbrowser.ui.utils
 
 object Constants {
     const val BROWSER_DATABASE = "browser_database"
+    const val TABS_TABLE = "tabs"
+    const val HISTORY_TABLE = "history"
 }
