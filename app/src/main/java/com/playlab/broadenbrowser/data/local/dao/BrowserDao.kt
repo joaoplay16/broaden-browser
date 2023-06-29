0e61acd5822f8425f7fd753f97d1a74483aca88e
João Pedro <joaoplay16@gmail.com>
2023-06-28 22:17:11 -0300

    Create 'BrowserDao' interface

---
+++ app/src/main/java/com/playlab/broadenbrowser/data/local/dao/BrowserDao.kt
@@ -0,0 +1,21 @@
+package com.playlab.broadenbrowser.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.playlab.broadenbrowser.model.TabPage
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface BrowserDao {
+    @Query("SELECT * FROM tabs")
+    fun getTabPages(): Flow<List<TabPage>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertTabPage(tabPage: TabPage)
+
+    @Delete
+    suspend fun deleteTabPage(tabPage: TabPage)
+}
