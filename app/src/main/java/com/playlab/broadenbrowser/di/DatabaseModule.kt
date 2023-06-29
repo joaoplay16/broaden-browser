9f80f52db7d6e91152afd2d0f40af6c5f8cd7b28
João Pedro <joaoplay16@gmail.com>
2023-06-28 22:24:24 -0300

    Provide 'BrowserDatabase' instance

--- app/src/main/java/com/playlab/broadenbrowser/di/DatabaseModule.kt
+++ app/src/main/java/com/playlab/broadenbrowser/di/DatabaseModule.kt
@@ -5,8 +5,11 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.room.Room
+import com.playlab.broadenbrowser.data.local.BrowserDatabase
 import com.playlab.broadenbrowser.data.preferences.PreferencesDataStore
 import com.playlab.broadenbrowser.repository.PreferencesRepository
+import com.playlab.broadenbrowser.ui.utils.Constants.BROWSER_DATABASE
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -34,4 +37,16 @@ object DatabaseModule {
         dataStore: PreferencesDataStore
     ): PreferencesRepository =
         PreferencesRepository(dataStore)
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): BrowserDatabase {
+        return Room.databaseBuilder(
+            context,
+            BrowserDatabase::class.java,
+            BROWSER_DATABASE
+        ).build()
+    }
 }
