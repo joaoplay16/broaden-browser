e747efc80a3e1b6a775efce36047a991fde33356
João Pedro <joaoplay16@gmail.com>
2023-06-28 21:41:32 -0300

    Create an Android component class to be setup for injection with the standard Hilt Dagger Android components

---
+++ app/src/debug/java/com/playlab/broadenbrowser/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.playlab.broadenbrowser
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : ComponentActivity()
