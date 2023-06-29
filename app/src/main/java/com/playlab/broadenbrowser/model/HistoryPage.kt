e3e45b5c3f614e22eb22624dab5894366c0c8b08
João Pedro <joaoplay16@gmail.com>
2023-06-28 22:13:32 -0300

    Create class 'HistoryPage' that serve as a database model representation for the 'history' table

---
+++ app/src/main/java/com/playlab/broadenbrowser/model/HistoryPage.kt
@@ -0,0 +1,32 @@
+package com.playlab.broadenbrowser.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.playlab.broadenbrowser.ui.utils.Constants.HISTORY_TABLE
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity(tableName = HISTORY_TABLE)
+open class HistoryPage(
+    @PrimaryKey
+    override val id: Int,
+    override val title: String,
+    override val url: String,
+    override val timestamp: Long
+): Parcelable, Page {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Page
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id
+    }
+}
