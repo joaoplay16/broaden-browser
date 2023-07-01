ddb42e5108d26d0f1d273c5b9b4b2517b150a63f
João Pedro <joaoplay16@gmail.com>
2023-07-01 20:25:44 -0300

    Create mock with `TabPages` for testing

---
+++ app/src/debug/java/com/playlab/broadenbrowser/mocks/MockTabPages.kt
@@ -0,0 +1,30 @@
+package com.playlab.broadenbrowser.mocks
+
+import com.playlab.broadenbrowser.model.TabPage
+
+object MockTabPages {
+    val tab1 = TabPage(
+        1,
+        "Google",
+        "https://google.com",
+        0L
+    )
+    val tab2 = TabPage(
+        2,
+        "Bing",
+        "https://bing.com",
+        0L
+    )
+    val tab3 = TabPage(
+        3,
+        "Yahoo",
+        "https://yahoo.com",
+        0L
+    )
+    val tab4 = TabPage(
+        4,
+        "Duck Duck Go",
+        "https://duckduckgo.com",
+        0L
+    )
+}
