4b691f7c97321381052046feab373b21191d96eb
João Pedro <joaoplay16@gmail.com>
2023-07-05 21:04:36 -0300

    Add a spacing at the bottom of the tab list to make sure that the float action button doesn't interfere on the tab click

--- app/src/main/java/com/playlab/broadenbrowser/ui/components/BottomSheetContent.kt
+++ app/src/main/java/com/playlab/broadenbrowser/ui/components/BottomSheetContent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -156,6 +157,9 @@ fun TabsSection(
                     }
                 }
             )
+        }
+        item{
+            Spacer(modifier = Modifier.padding(64.dp))
         }
     }
 }
