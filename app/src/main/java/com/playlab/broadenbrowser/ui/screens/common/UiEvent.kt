39437c6e12fc5fc34b398d794cd074648d174744
João Pedro <joaoplay16@gmail.com>
2023-07-06 23:05:44 -0300

    Refactor: remove the params of the event `OnNewTab` as its responsibility is only to trigger the event of adding a new tab. Additionally, add the new event `OnSaveTab` which is in charge of saving the tab

--- app/src/main/java/com/playlab/broadenbrowser/ui/screens/common/UiEvent.kt
+++ app/src/main/java/com/playlab/broadenbrowser/ui/screens/common/UiEvent.kt
@@ -10,7 +10,8 @@ sealed class UiEvent {
     data class OnEnableDarkTheme(val enabled: Boolean): UiEvent()
     data class OnSetSearchMechanism(val searchMechanism: SearchMechanism): UiEvent()
     data class OnSetAsDefaultBrowser(val isDefaultBrowser: Boolean): UiEvent()
-    data class OnNewTab(val tabPage: TabPage): UiEvent()
+    object OnNewTab: UiEvent()
+    data class OnSaveTab(val tabPage: TabPage): UiEvent()
     data class OnCloseTabs(val tabPages: List<TabPage>): UiEvent()
     object OnCloseAllTabs : UiEvent()
 }
