3446054e48eeabaa0ca6b123a19be24aaf92aeac
João Pedro <joaoplay16@gmail.com>
2023-07-08 21:51:22 -0300

    Remove equals and hashcode overrides and convert `TabPage` to a data class as it generates these methods based on all properties. Improves the readability and maintainability reducing the amount of code making more easier to read

--- app/src/main/java/com/playlab/broadenbrowser/model/TabPage.kt
+++ app/src/main/java/com/playlab/broadenbrowser/model/TabPage.kt
@@ -8,25 +8,10 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 @Entity(tableName = TABS_TABLE)
-open class TabPage(
+data class TabPage(
     @PrimaryKey(autoGenerate = true)
     override val id: Int = 0,
     override val title: String,
     override val url: String,
     override val timestamp: Long
-): Parcelable, Page {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Page
-
-        if (id != other.id) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return id
-    }
-}
+): Parcelable, Page
