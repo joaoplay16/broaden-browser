46756b0ce3ac1103224effe7d6d3a15bb3e10a0b
João Pedro <joaoplay16@gmail.com>
2023-07-22 22:02:36 -0300

    Refactor: remove boilerplate code from `HistoryPge`

--- app/src/main/java/com/playlab/broadenbrowser/model/HistoryPage.kt
+++ app/src/main/java/com/playlab/broadenbrowser/model/HistoryPage.kt
@@ -8,25 +8,10 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 @Entity(tableName = HISTORY_TABLE)
-open class HistoryPage(
+data class HistoryPage(
     @PrimaryKey
     override val id: Int,
     override val title: String,
     override val url: String,
     override val timestamp: Long
-): Parcelable, Page {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Page
-
-        if (id != other.id) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return id
-    }
-}
+): Parcelable, Page
