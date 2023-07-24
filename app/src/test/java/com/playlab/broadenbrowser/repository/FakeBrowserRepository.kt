f5ca1a2b98971cda7c8160983cc832932497196c
João Pedro <joaoplay16@gmail.com>
2023-07-24 12:14:05 -0300

    Implement history handling functions in `FakeBrowserRepository`

--- app/src/test/java/com/playlab/broadenbrowser/repository/FakeBrowserRepository.kt
+++ app/src/test/java/com/playlab/broadenbrowser/repository/FakeBrowserRepository.kt
@@ -1,5 +1,6 @@
 package com.playlab.broadenbrowser.repository
 
+import com.playlab.broadenbrowser.model.HistoryPage
 import com.playlab.broadenbrowser.model.TabPage
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -7,6 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 class FakeBrowserRepository : BrowserRepository {
 
     private var tabPages: MutableStateFlow<List<TabPage>> = MutableStateFlow(emptyList())
+    private var history: MutableStateFlow<List<HistoryPage>> = MutableStateFlow(emptyList())
 
     override fun getTabs(): Flow<List<TabPage>> {
         return tabPages
@@ -36,5 +38,35 @@ class FakeBrowserRepository : BrowserRepository {
 
     override suspend fun getTab(id: Long): TabPage? {
         return tabPages.value.find { it.id.toLong() == id }
+    }
+
+    override suspend fun insertHistoryPage(historyPage: HistoryPage): Long {
+        history.value = history.value.plus(historyPage)
+        return history.value.size.toLong()
+    }
+
+    override fun getHistory(): Flow<List<HistoryPage>> {
+        return history
+    }
+
+    override suspend fun deleteHistoryPages(historyPages: List<HistoryPage>) {
+        this.history.value = this.history.value.minus(historyPages.toSet())
+    }
+
+    override suspend fun deleteAllHistoryPages() {
+        this.history.value = emptyList()
+    }
+
+    override suspend fun editHistoryPage(historyPage: HistoryPage): Int {
+        val tabToEditIndex = history.value.indexOfFirst { it.id == historyPage.id }
+        val listWithTheModifiedHistoryPage = history.value.toMutableList()
+        listWithTheModifiedHistoryPage[tabToEditIndex] = historyPage
+        history.value = listWithTheModifiedHistoryPage
+
+        return if (history.value.contains(historyPage)) 1 else 0
+    }
+
+    override suspend fun getHistoryPage(id: Long): HistoryPage? {
+        return history.value.find { it.id.toLong() == id }
     }
 }
