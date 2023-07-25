05f3a2ff98a263c5283303e59205a952744e37a5
João Pedro <joaoplay16@gmail.com>
2023-07-25 11:30:41 -0300

    Add the event `OnDeleteHistoryPages` in `UIEvent`

--- app/src/main/java/com/playlab/broadenbrowser/ui/screens/common/UiEvent.kt
+++ app/src/main/java/com/playlab/broadenbrowser/ui/screens/common/UiEvent.kt
@@ -5,17 +5,17 @@ import com.playlab.broadenbrowser.model.TabPage
 import com.playlab.broadenbrowser.ui.utils.SearchMechanism
 
 sealed class UiEvent {
-    data class OnAllowJavascript(val allowed: Boolean): UiEvent()
-    data class OnEnableFullscreen(val enabled: Boolean): UiEvent()
-    data class OnEnableStartInFullscreen(val enabled: Boolean): UiEvent()
-    data class OnEnableDarkTheme(val enabled: Boolean): UiEvent()
-    data class OnSetSearchMechanism(val searchMechanism: SearchMechanism): UiEvent()
-    data class OnSetAsDefaultBrowser(val isDefaultBrowser: Boolean): UiEvent()
-    data class OnNewTab(val tabPage: TabPage?): UiEvent()
-    data class OnSaveTab(val tabPage: TabPage): UiEvent()
-    data class OnEditTab(val tabPage: TabPage): UiEvent()
-    data class OnCloseTabs(val tabPages: List<TabPage>): UiEvent()
-    data class OnSaveHistoryPage(val historyPage: HistoryPage) : UiEvent()
-
+    data class OnAllowJavascript(val allowed: Boolean) : UiEvent()
+    data class OnEnableFullscreen(val enabled: Boolean) : UiEvent()
+    data class OnEnableStartInFullscreen(val enabled: Boolean) : UiEvent()
+    data class OnEnableDarkTheme(val enabled: Boolean) : UiEvent()
+    data class OnSetSearchMechanism(val searchMechanism: SearchMechanism) : UiEvent()
+    data class OnSetAsDefaultBrowser(val isDefaultBrowser: Boolean) : UiEvent()
+    data class OnNewTab(val tabPage: TabPage?) : UiEvent()
+    data class OnSaveTab(val tabPage: TabPage) : UiEvent()
+    data class OnEditTab(val tabPage: TabPage) : UiEvent()
+    data class OnCloseTabs(val tabPages: List<TabPage>) : UiEvent()
     object OnCloseAllTabs : UiEvent()
+    data class OnSaveHistoryPage(val historyPage: HistoryPage) : UiEvent()
+    data class OnDeleteHistoryPages(val historyPages: List<HistoryPage>) : UiEvent()
 }
