66039866cb26dd299c3d7d46754a3f7dd58de52c
João Pedro <joaoplay16@gmail.com>
2023-08-06 22:02:14 -0300

    Create a test suit for the `SaveTabHistoryEntryUseCase`
    Test the tab history entry saving

---
+++ app/src/test/java/com/playlab/broadenbrowser/domain/TestSaveTabHistoryEntryUseCase.kt
@@ -0,0 +1,53 @@
+package com.playlab.broadenbrowser.domain
+
+import com.google.common.truth.Truth.assertThat
+import com.playlab.broadenbrowser.MainCoroutineRule
+import com.playlab.broadenbrowser.mocks.MockHistoryPages.historyPage1
+import com.playlab.broadenbrowser.mocks.MockHistoryPages.historyPage2
+import com.playlab.broadenbrowser.mocks.MockTabPages.tab4
+import com.playlab.broadenbrowser.repository.FakeBrowserRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class TestSaveTabHistoryEntryUseCase {
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var saveEditHistoryPageUseCase: SaveTabHistoryEntryUseCase
+    private lateinit var repository: FakeBrowserRepository
+
+    @Before
+    fun before(){
+        repository = FakeBrowserRepository()
+        saveEditHistoryPageUseCase = SaveTabHistoryEntryUseCase(repository)
+    }
+
+    @Test
+    fun `saving a tab history entry use case`() =
+        runTest {
+            val tabId = repository.insertTabPage(tab4)
+
+            repository.insertHistoryPage(historyPage1)
+            repository.insertHistoryPage(historyPage2)
+
+            val history = repository.getHistory().first()
+
+            history.forEach {
+                saveEditHistoryPageUseCase(
+                    tabPage = tab4.copy(id = tabId.toInt()), // tab with updated the id
+                    historyPage = it
+                )
+            }
+
+            val tabHistory = repository.getTabHistory(tabId).first()
+
+            assertThat(tabHistory).isNotEmpty()
+            assertThat(tabHistory.size).isEqualTo(2)
+        }
+}
