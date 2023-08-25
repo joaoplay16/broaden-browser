69c4e6103fb35432dde324e095a3416d189ed577
João Pedro <joaoplay16@gmail.com>
2023-08-24 21:10:15 -0300

    Add `Bookmark` class to database entity array

--- app/src/main/java/com/playlab/broadenbrowser/data/local/BrowserDatabase.kt
+++ app/src/main/java/com/playlab/broadenbrowser/data/local/BrowserDatabase.kt
@@ -3,12 +3,18 @@ package com.playlab.broadenbrowser.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.playlab.broadenbrowser.data.local.dao.BrowserDao
+import com.playlab.broadenbrowser.model.Bookmark
 import com.playlab.broadenbrowser.model.HistoryPage
 import com.playlab.broadenbrowser.model.TabHistoryEntry
 import com.playlab.broadenbrowser.model.TabPage
 
 @Database(
-    entities = [TabPage::class, HistoryPage::class, TabHistoryEntry::class],
+    entities = [
+        TabPage::class,
+        HistoryPage::class,
+        TabHistoryEntry::class,
+        Bookmark::class
+    ],
     version = 1
 )
 abstract class BrowserDatabase : RoomDatabase() {
