e77c73bc3b9ff9d45a22039beec29f0364d99c6d
João Pedro <joaoplay16@gmail.com>
2023-08-25 23:57:41 -0300

    Create `TabHistoryManager` class for managing tab navigation history
    This commit introduces the TabHistory class responsible for managing the navigation history of tabs. This utility class provides functions to track and navigate through the history of visited tabs within the application. The TabHistory class is designed to simplify the management of tab history and improve the user experience when navigating between tabs.
    
    Also add a test suite to test functionality

---
+++ app/src/main/java/com/playlab/broadenbrowser/model/TabHistoryManager.kt
@@ -0,0 +1,39 @@
+package com.playlab.broadenbrowser.model
+
+class TabHistoryManager {
+    private var currentIndex: Int = 0
+    private var historyPages: List<HistoryPage> = mutableListOf()
+
+    fun setCurrentHistoryEntry(
+        currentHistoryEntry: HistoryPage? = null,
+    ) {
+        val initialEntryIndex = historyPages.indexOf(currentHistoryEntry)
+        currentIndex = if (initialEntryIndex > 0) initialEntryIndex else 0
+    }
+
+    fun updateTabHistory(historyPages: List<HistoryPage>) {
+        this.historyPages = historyPages
+    }
+
+    fun hasNext(): Boolean {
+        return currentIndex >= 0 && currentIndex < historyPages.size - 1
+    }
+
+    fun hasPrevious(): Boolean {
+        return currentIndex > 0 && historyPages.isNotEmpty()
+    }
+
+    fun nextTabHistoryEntry(): HistoryPage? {
+        return if (hasNext()) {
+            currentIndex++
+            historyPages[currentIndex]
+        } else null
+    }
+
+    fun previousTabHistoryEntry(): HistoryPage? {
+        return if (hasPrevious()) {
+            currentIndex--
+            historyPages[currentIndex]
+        } else null
+    }
+}
---
+++ app/src/test/java/com/playlab/broadenbrowser/model/TestTabHistoryManager.kt
@@ -0,0 +1,71 @@
+package com.playlab.broadenbrowser.model
+
+import com.google.common.truth.Truth.assertThat
+import com.playlab.broadenbrowser.mocks.MockHistoryPages.browserHistory
+import com.playlab.broadenbrowser.mocks.MockHistoryPages.historyPage1
+import com.playlab.broadenbrowser.mocks.MockHistoryPages.historyPage2
+import com.playlab.broadenbrowser.mocks.MockHistoryPages.historyPage3
+import com.playlab.broadenbrowser.mocks.MockHistoryPages.historyPage4
+import org.junit.Test
+
+class TestTabHistoryManager {
+
+    @Test
+    fun `test navigate forward`(){
+        val tabHistoryManager = TabHistoryManager()
+
+        tabHistoryManager.updateTabHistory(browserHistory)
+
+        assertThat(tabHistoryManager.nextTabHistoryEntry()).isEqualTo(historyPage2)
+        assertThat(tabHistoryManager.nextTabHistoryEntry()).isEqualTo(historyPage3)
+        assertThat(tabHistoryManager.nextTabHistoryEntry()).isEqualTo(historyPage4)
+        assertThat(tabHistoryManager.nextTabHistoryEntry()).isNull()
+    }
+
+    @Test
+    fun `test navigate back`(){
+        val tabHistoryManager = TabHistoryManager()
+
+        tabHistoryManager.updateTabHistory(browserHistory)
+        tabHistoryManager.setCurrentHistoryEntry(historyPage4)
+
+        assertThat(tabHistoryManager.previousTabHistoryEntry()).isEqualTo(historyPage3)
+        assertThat(tabHistoryManager.previousTabHistoryEntry()).isEqualTo(historyPage2)
+        assertThat(tabHistoryManager.previousTabHistoryEntry()).isEqualTo(historyPage1)
+        assertThat(tabHistoryManager.previousTabHistoryEntry()).isNull()
+    }
+
+    @Test
+    fun `test navigate back an forth`(){
+
+        val tabHistoryManager = TabHistoryManager()
+
+        tabHistoryManager.updateTabHistory(browserHistory)
+        tabHistoryManager.setCurrentHistoryEntry(historyPage3)
+
+        assertThat(tabHistoryManager.previousTabHistoryEntry()).isEqualTo(historyPage2)
+        assertThat(tabHistoryManager.previousTabHistoryEntry()).isEqualTo(historyPage1)
+        assertThat(tabHistoryManager.nextTabHistoryEntry()).isEqualTo(historyPage2)
+        assertThat(tabHistoryManager.previousTabHistoryEntry()).isEqualTo(historyPage1)
+        assertThat(tabHistoryManager.previousTabHistoryEntry()).isNull()
+    }
+
+    @Test
+    fun `test tab history update`(){
+
+        val tabHistoryManager = TabHistoryManager()
+
+        tabHistoryManager.updateTabHistory(browserHistory)
+        tabHistoryManager.setCurrentHistoryEntry(historyPage3)
+
+        assertThat(tabHistoryManager.previousTabHistoryEntry()).isEqualTo(historyPage2)
+        assertThat(tabHistoryManager.previousTabHistoryEntry()).isEqualTo(historyPage1)
+        assertThat(tabHistoryManager.nextTabHistoryEntry()).isEqualTo(historyPage2)
+
+        tabHistoryManager.setCurrentHistoryEntry(historyPage2)
+        // Simulating switch to other tab history
+        tabHistoryManager.updateTabHistory(listOf(historyPage3, historyPage2, historyPage1))
+
+        assertThat(tabHistoryManager.nextTabHistoryEntry()).isEqualTo(historyPage1)
+    }
+}
