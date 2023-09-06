a13bbc96e713e0901ffca5d824ed5ef294b1350b
João Pedro <joaoplay16@gmail.com>
2023-09-05 23:18:02 -0300

    Refactor `TabPage` to store the tab history as `ByteArray`
    Also overrides the `equals` an `hashcode` methods due the array property "is recommended".

--- app/src/main/java/com/playlab/broadenbrowser/model/TabPage.kt
+++ app/src/main/java/com/playlab/broadenbrowser/model/TabPage.kt
@@ -1,6 +1,7 @@
 package com.playlab.broadenbrowser.model
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.playlab.broadenbrowser.ui.utils.Constants.TABS_TABLE
@@ -13,5 +14,32 @@ data class TabPage(
     override val id: Int = 0,
     override val title: String,
     override val url: String,
-    override val timestamp: Long
-): Parcelable, Page
+    override val timestamp: Long,
+    @ColumnInfo(typeAffinity = ColumnInfo.BLOB)
+    val historyByteArray: ByteArray? = null
+): Parcelable, Page {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TabPage
+
+        if (id != other.id) return false
+        if (title != other.title) return false
+        if (url != other.url) return false
+        if (historyByteArray != null) {
+            if (other.historyByteArray == null) return false
+            if (!historyByteArray.contentEquals(other.historyByteArray)) return false
+        } else if (other.historyByteArray != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + title.hashCode()
+        result = 31 * result + url.hashCode()
+        result = 31 * result + (historyByteArray?.contentHashCode() ?: 0)
+        return result
+    }
+}
